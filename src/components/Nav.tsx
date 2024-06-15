e1e107edfbc874432f653c585846bb29fc1d885e
zach <zachflentgewong@gmail.com>
2024-06-15 15:37:48 -0700

    created the /new/setup endpoint for when user submits new sku form, created headers component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,16 +3,30 @@ import { CiMail } from "react-icons/ci";
 import { RiLightbulbLine } from "react-icons/ri";
 import { IoHelp } from "react-icons/io5";
 
-
 export default function Nav() {
   return (
     <div className="w-48 h-screen bg-neutral-100 flex flex-col items-center pt-1 cursor-pointer">
-      <div hx-get="/" hx-target="body" hx-swap="outerHTML transition:true" hx-push-url="true"><img src="/bby-logo.png" alt="" className="w-[50px]" /></div>
+      <div
+        hx-get="/"
+        hx-target="body"
+        hx-swap="outerHTML transition:true"
+        hx-push-url="true"
+      >
+        <img src="/bby-logo.png" alt="" className="w-[50px]" />
+      </div>
       <div className="flex flex-col gap-3">
-		<NavLink link=" /new/" icon={<RiLightbulbLine color="teal"/>} title="Create New SKUs" />
-		<NavLink link="/contact/" icon={<CiMail color="teal"/>} title="Contact" />
-		<NavLink link="/faq/" icon={<IoHelp color="teal"/>} title="FAQs" />
-	  </div>
+        <NavLink
+          link=" /new/"
+          icon={<RiLightbulbLine color="teal" />}
+          title="Create New SKUs"
+        />
+        <NavLink
+          link="/contact/"
+          icon={<CiMail color="teal" />}
+          title="Contact"
+        />
+        <NavLink link="/faq/" icon={<IoHelp color="teal" />} title="FAQs" />
+      </div>
     </div>
   );
 }
--- src/components/ui/NavCard.tsx
+++ src/components/ui/NavCard.tsx
@@ -11,7 +11,7 @@ export default function NavCard({ title, icon, link }: Props) {
       hx-get={link}
       hx-target="body"
       hx-swap="outerHTML transition:true"
-      className="flex flex-col justify-center items-center w-32 h-32 border rounded-md shadow-sm hover:shadow-lg hover:scale-105 transition-all text-base font-normal tracking-tight gap-4 cursor-pointer"
+      className="flex flex-col justify-center items-center w-32 h-32 border rounded-md shadow-sm hover:shadow-lg hover:scale-105 transition-all duration-0 ease-in text-base font-normal tracking-tight gap-4 cursor-pointer"
     >
       {title} {icon}
     </div>
