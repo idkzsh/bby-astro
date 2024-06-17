d3f64c3fd48f6197d5ceb04b45ea1f035a1d6c3f
zach <zachflentgewong@gmail.com>
2024-06-16 23:37:42 -0700

    ported the astro setup inputs to a react component

--- src/components/TableHeaders.tsx
+++ src/components/TableHeaders.tsx
@@ -4,10 +4,11 @@ interface Props {
 
 export default function TableHeaders({ list }: Props) {
   return (
-    <div className="flex flex-col w-48 gap-0.5">
+    <div className="flex flex-col w-[160px]">
+      <div className="h-4 border-r border-b text-xs text-center font-bold">Attribute</div>
       {
-        Object.entries(list).map(([key, val]) => (
-          <div className="flex h-10 bg-neutral-50 rounded-md text-xs font-bold items-center px-2 py-4 w-[160px]">
+        Object.entries(list).map(([key]) => (
+          <div className="flex h-10 bg-neutral-50 px-2 border-r border-b text-xs font-bold items-center py-4">
             {key}
           </div>
         ))
--- src/components/TableInputs.tsx
+++ src/components/TableInputs.tsx
@@ -1,19 +1,98 @@
-interface Props {
-  list: Record<string, string>;
-  columns: number;
+interface inputObj {
+  name: string;
+  type:
+  | "text"
+  | "password"
+  | "number"
+  | "checkbox"
+  | "radio"
+  | "select"
+  | "date";
+  maxLen: number;
+  minLen: number;
+  step: number;
+  required: boolean;
+  min: number;
+  max: number;
+  options: object;
 }
 
+interface Props {
+  formName: string;
+  columns: number;
+  data: inputObj[];
+}
 
+const today = new Date().toISOString().split("T")[0];
 
-export default function TableInputs({ columns, list }: Props) {
-
-  console.log(columns)
+export default function TableInputs({ columns, data }: Props) {
   return (
-    <div className="flex">  {/* Wrapper element to contain the mapped array */}
-      {Array.from({ length: columns }).map((_, index) => (
-        <div key={index} className="w-40 border-r">Column {index}BOOM</div>
+    <form className="flex" action="/compliance" method="POST">
+      {
+        Array.from({ length: columns }).map((_, index) => (
+          <>
+            <input type="hidden" name="columns" value={columns} />
+            <div className="w-40">
+              <div className="h-4 text-xs text-center font-bold border-b">
+                SKU {index + 1}
+              </div>
+              {data.map((item) => (
+                <div className="flex h-10 border-b border-r justify-center items-center text-sm">
 
-      ))}
-    </div>
+                  {item.type === "text" && (
+                    <input
+                      className="w-[80%] border bg-background h-6 text-sm"
+                      name={item.name + `-${index}`}
+                      id={item.name + `-${index}`}
+                      type={item.type}
+                      required={item.required}
+                      minLength={item.minLen}
+                      maxLength={item.maxLen}
+                    />
+                  )}
+                  {item.type === "number" && (
+                    <input
+                      className="w-[80%] border bg-background h-6 text-sm"
+                      name={item.name + `-${index}`}
+                      id={item.name + `-${index}`}
+                      type={item.type}
+                      step={item.step}
+                      max={item.max}
+                      required={item.required}
+                    />
+                  )}
+                  {item.type === "select" && (
+                    <select
+                      name={item.name + `-${index}`}
+                      id={item.name + `-${index}`}
+                      className="w-[80%] border bg-background h-6 text-sm"
+                      required={item.required}
+
+                    >
+                      <option />
+                      {Object.entries(item.options).map(([key, val]) => (
+                        <option value={key}>{val}</option>
+                      ))}
+                    </select>
+                  )}
+                  {item.type === "date" && (
+                    <input
+                      className="w-[80%] border bg-background h-6 text-sm text-primary"
+                      type="date"
+                      min={today}
+                      name={item.name + `-${index}`}
+                      id={item.name + `-${index}`}
+                      required={item.required}
+                      pattern="\d{4}-\d{2}-\d{2}"
+
+                    />
+                  )}
+                </div>
+              ))}
+            </div>
+          </>
+        ))
+      }
+    </form>
   );
 }
