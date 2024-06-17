1a6ab713fd3e01fc983332cdc65babefd36254e8
zach <zachflentgewong@gmail.com>
2024-06-16 17:04:32 -0700

    added setup table inputs

--- src/components/TableHeaders.tsx
+++ src/components/TableHeaders.tsx
@@ -7,7 +7,7 @@ export default function TableHeaders({ list }: Props) {
     <div className="flex flex-col w-48 gap-0.5">
       {
         Object.entries(list).map(([key, val]) => (
-          <div className="flex h-10 bg-neutral-50 rounded-md text-xs font-bold items-center px-2 w-[160px]">
+          <div className="flex h-10 bg-neutral-50 rounded-md text-xs font-bold items-center px-2 py-4 w-[160px]">
             {key}
           </div>
         ))
--- src/components/TableInputs.tsx
+++ src/components/TableInputs.tsx
@@ -1,6 +1,19 @@
 interface Props {
   list: Record<string, string>;
-  columns: Number;
+  columns: number;
 }
 
-export default function TableInputs() { }
+
+
+export default function TableInputs({ columns, list }: Props) {
+
+  console.log(columns)
+  return (
+    <div className="flex">  {/* Wrapper element to contain the mapped array */}
+      {Array.from({ length: columns }).map((_, index) => (
+        <div key={index} className="w-40 border-r">Column {index}BOOM</div>
+
+      ))}
+    </div>
+  );
+}
