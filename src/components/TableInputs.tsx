d04254dd85c07b37a86ad27f5996313a46dedbd1
zach <zachflentgewong@gmail.com>
2024-06-16 23:46:20 -0700

    not sure what the change was

--- src/components/TableInputs.tsx
+++ src/components/TableInputs.tsx
@@ -84,7 +84,6 @@ export default function TableInputs({ columns, data }: Props) {
                       id={item.name + `-${index}`}
                       required={item.required}
                       pattern="\d{4}-\d{2}-\d{2}"
-
                     />
                   )}
                 </div>
