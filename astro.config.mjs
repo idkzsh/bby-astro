3fec9d06fbea8a0be28c02ffd0856e540f94af58
zach <zachflentgewong@gmail.com>
2024-09-23 19:30:29 -0700

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
 
 import htmx from "astro-htmx";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), htmx()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel(),
 });
+
